0d0b738b062eeeb2d3315d98a0fdebca589fffe5
yudai watanabe <judas.wata@gmail.com>
2018-06-12 11:26:09 +0900

    Initial Commit

---
+++ footaball 2018WC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  footaball 2018WC
+//
+//  Created by watanabe-yudai on 2018/06/12.
+//  Copyright © 2018年 watanabe-yudai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
